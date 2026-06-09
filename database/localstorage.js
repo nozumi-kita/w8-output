da0eba46392c357c7a78333576699d7bf1c25a05
nozumi <minirogo2052.igo@gmail.com>
2026-06-09 16:06:00 +0900

    LocalStorageを追加。

---
+++ database/localstorage.js
@@ -0,0 +1,27 @@
+const formLogin = document.getElementById("form-login");
+
+// ログイン時に情報保存
+formLogin.addEventListener("submit", () => {
+    const formData = new FormData(formLogin);
+
+    const email = formData.get('email');
+    const password = formData.get('password');
+    localStorage.setItem('email', email);
+    localStorage.setItem('password', password);
+})
+
+// ページ読み込み時に実行
+document.addEventListener('DOMContentLoaded', () => {
+    const email = localStorage.getItem('email');
+    const password = localStorage.getItem('password');
+    
+    document.getElementById('email').value = email;
+    document.getElementById('password').value = password;
+});
+
+// 今回ローカルストレージに保存したデータを削除
+const deleteStorageData = () => {
+    localStorage.removeItem('email');
+    localStorage.removeItem('password');
+    location.reload();
+}
